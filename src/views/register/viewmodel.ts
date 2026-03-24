85887bdd358144cd08960071fc9d9ae37ef407a6
Bruno Da Silva <brunogustavo68@gmail.com>
2026-03-24 19:54:04 +0100

    style: format type declaration for better readability

--- src/views/register/viewmodel.ts
+++ src/views/register/viewmodel.ts
@@ -2,7 +2,10 @@ import { useNavigation } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import type { AuthStackParamList } from '../../navigation/types';
 
-type RegisterNavProp = NativeStackNavigationProp<AuthStackParamList, 'Register'>;
+type RegisterNavProp = NativeStackNavigationProp<
+  AuthStackParamList,
+  'Register'
+>;
 
 export const useViewModel = () => {
   const navigation = useNavigation<RegisterNavProp>();
